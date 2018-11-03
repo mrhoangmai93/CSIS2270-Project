1689b5b0f675935e5802c871d27c6f357bd248a4
Hoang Mai <hoang.mai@smartt.com>
2018-11-03 14:36:01 -0700

    add network implementation

--- app/src/main/java/com/example/xer/finalproject/activity/MainActivity.java
+++ app/src/main/java/com/example/xer/finalproject/activity/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.xer.finalproject;
+package com.example.xer.finalproject.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.xer.finalproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
